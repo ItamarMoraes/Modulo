1fc02629106a57aed626f749d9a0cbad64a8e834
Itamar <ivoitamar@gmail.com>
2021-11-09 10:26:29 -0300

    Substituição das aspas duplas por aspas simples.

--- js/main.mjs
+++ js/main.mjs
@@ -1,1 +1,5 @@
-﻿
+﻿import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Itamar', 46));
+console.log(mostraCidade('Itamar', 'Arcoverde'));
+console.log(mostraHobby('Itamar', 'Ciclismo'));
